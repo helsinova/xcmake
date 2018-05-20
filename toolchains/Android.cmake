78958319d54aab00eb25c6c18aaebbdfa998f3ca
Michael Ambrus <michael@helsinova.se>
2018-05-20 19:03:48 +0200

    Full feature

--- toolchains/Android.cmake
+++ toolchains/Android.cmake
@@ -1,6 +1,8 @@
 include (CMakeForceCompiler)
 set(CMAKE_SYSTEM_NAME Generic)
 
+# Tool-chain file which auto-deducts options from x-tool in PATH.
+
 set(XTOOL_PREFIX
 	arm-linux-androideabi
 	CACHE STRING
@@ -17,6 +19,7 @@ endif(NOT GCC_EXECUTABLE)
 
 set(CMAKE_C_COMPILER_TARGET ${X_PREFIX})
 
+#Autodetect if tool-chain can build for Android
 execute_process(
 	COMMAND ${X_PREFIX}gcc -dM -E -
 	INPUT_FILE /dev/null
@@ -41,22 +44,63 @@ endif (NOT ANDROID_TARGET_TEST STREQUAL "")
 set(HAVE_ANDROID_OS
 	${HAVE_ANDROID_OS_DEFAULT}
 	CACHE BOOL
-	"Is this build intended for a Android system?")
+	"Is this build intended for an Android system?")
 
 if (HAVE_ANDROID_OS)
 	message(STATUS "Going to build for Android target" )
-	set(CMAKE_EXTRA_C_FLAGS "${CMAKE_EXTRA_C_FLAGS} -fPIE -pie -DHAVE_ANDROID_OS")
 
-# Autodetect SYSROOT in NDK
+	# Autodetect SYSROOT in NDK
 	execute_process(
 		COMMAND which "${X_PREFIX}gcc"
 		OUTPUT_VARIABLE NDK_GCC_FULLPATH
 	)
+	message(STATUS "NDK_GCC_FULLPATH: ${NDK_GCC_FULLPATH}" )
 	execute_process(
 		COMMAND ${PROJECT_SOURCE_DIR}/cmake/host/android_sysroot.sh
 			${NDK_GCC_FULLPATH}
 			OUTPUT_VARIABLE __DETECTED_SYSROOT
 	)
 	string(STRIP "${__DETECTED_SYSROOT}" DETECTED_SYSROOT)
+	message(STATUS "DETECTED_SYSROOT: ${DETECTED_SYSROOT}" )
+
+	if (NOT DETECTED_SYSROOT STREQUAL "")
+		set(CMAKE_SYSROOT "${DETECTED_SYSROOT}")
+	endif (NOT DETECTED_SYSROOT STREQUAL "")
+
+	# Set *C_FLAGS section but avoid build-up
+	#
+	# CMAKE_EXTRA_C_FLAGS: Independent of CMAKE_C_FLAGS so they can be
+	#                      concatenated, but concatenation must be done in
+	#                      root CMakeLists.txt
+	if (NOT HAVE_TOOLFILE_SET_EXTRA_C_FLAGS)
+		set(CMAKE_EXTRA_C_FLAGS "${CMAKE_EXTRA_C_FLAGS} -fPIE -pie -DHAVE_ANDROID_OS")
+
+		# If tool-chain isn't specifically for Android, but user configured
+		# HAVE_ANDROID_OS anyway, at lest set -DANDROID
+		if (ANDROID_TARGET_TEST STREQUAL "")
+			set(CMAKE_EXTRA_C_FLAGS "${CMAKE_EXTRA_C_FLAGS} -DANDROID")
+			message(
+				WARNING
+				"Non-Android tool-chain used for building Android: ${ANDROID_TARGET_TEST}"
+			)
+		endif (NOT ANDROID_TARGET_TEST STREQUAL "")
+
+		# Append --sysroot option. Needed for this CMAKE_SYSTEM_NAME
+		if (NOT SYSROOT STREQUAL "")
+			set(CMAKE_EXTRA_C_FLAGS "${CMAKE_EXTRA_C_FLAGS} --sysroot=${CMAKE_SYSROOT}")
+		endif (NOT SYSROOT STREQUAL "")
+
+		set	(HAVE_TOOLFILE_SET_EXTRA_C_FLAGS 1)
+	endif (NOT HAVE_TOOLFILE_SET_EXTRA_C_FLAGS)
+
+	# Convenience version of the above.
+	# WARNING: Could be overwritten by user-configuration which may not be what you
+	# want. See CMAKE_EXTRA_C_FLAGS for a better way of handling C_FLAGS
+	if (NOT HAVE_TOOLFILE_SET_C_FLAGS)
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_EXTRA_C_FLAGS}")
+
+		set	(HAVE_TOOLFILE_SET_C_FLAGS 1)
+	endif (NOT HAVE_TOOLFILE_SET_C_FLAGS)
+
 endif (HAVE_ANDROID_OS)
 
